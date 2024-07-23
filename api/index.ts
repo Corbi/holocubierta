3457791e649ac5324b7640f44ae64b53b8d2a519
Corbi <corbi_92@hotmail.es>
2024-07-23 09:59:17 +0200

    Prueba

--- api/index.ts
+++ api/index.ts
@@ -3,14 +3,14 @@ import axios from 'axios';
 
 // Define el manejador de la función para Vercel
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-	
-	console.error("Entra a la aplicación");
+
+  console.error("Entra a la aplicación");
 
   const { texto } = req.query;
   const apiKeyWizModel = process.env.API_KEY;  // Reemplaza esto con tu clave de API de WizModel
 
-	console.error("apiKeyWizModel");
-    
+  console.error("apiKeyWizModel");
+
   // Verifica si el parámetro texto está definido
   if (!texto || typeof texto !== 'string') {
     return res.status(400).json({ error: 'Parámetro "texto" es requerido' });
@@ -30,25 +30,10 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
   };
 
   try {
-	const wizModelResponse = await axios.post(wizModelUrl, wizModelPayload, { headers: wizModelHeaders });
+    const wizModelResponse = await axios.post(wizModelUrl, wizModelPayload, { headers: wizModelHeaders });
 
-    if (!wizModelResponse.data || !wizModelResponse.data.images || !wizModelResponse.data.images.length) {
-      throw new Error('La respuesta de la API de WizModel no contiene imágenes');
-    }
-
-    const base64Image = wizModelResponse.data.images[0];
-
-    // Asegúrate de que la cadena base64 no contiene encabezados como "data:image/png;base64,"
-    const base64Data = base64Image.replace(/^data:image\/png;base64,/, '');
-
-    // Decodifica la imagen base64
-    const imageBuffer = Buffer.from(base64Data, 'base64');
-
-    // Configura el encabezado de tipo de contenido como image/png
-    res.setHeader('Content-Type', 'image/png');
-    
-    // Envía el buffer de la imagen como respuesta
-    res.send(imageBuffer);
+    // Devuelve la respuesta completa de la API
+    res.status(200).json(wizModelResponse.data);
   } catch (error) {
     console.error('Error al generar o reescalar la imagen:', error.message || error);
     res.status(500).json({ error: 'Error al generar o reescalar la imagen' });
